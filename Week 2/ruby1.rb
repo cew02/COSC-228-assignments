7f88c12118ca5e3fc9ede8b9ea480e295965a0c8
Cody Wiley <cwiley.cs@gmail.com>
2024-01-16 11:21:21 -0500

    Update ruby1.rb

--- Week 2/ruby1.rb
+++ Week 2/ruby1.rb
@@ -2,4 +2,4 @@ puts "This is easy"
 puts "Hello World"
 puts "I have to go to work at 5AM Tomorrow #Sad"
 puts "I like python better but this is fine"
-5.times {print "Now I need to know how to space these."}
+7.times {print "Now I need to know how to space these."}
