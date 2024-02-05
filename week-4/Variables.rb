7c5e6a9594c16b068fd8e178acb03bcfbb3f9c91
Cody Wiley <cwiley.cs@gmail.com>
2024-02-05 12:35:23 -0500

    Create Variables.rb

---
+++ week-4/Variables.rb
@@ -0,0 +1,28 @@
+# A variable is a name assigned to an object
+
+# Sometimes called pointers
+
+# name = "Boris"
+# age = 31
+# cost = 3.99 + 4.99
+
+# Cannot contain spaces
+
+# my_favorite_holiday 
+# also called "snake_case" because it looks like a snake
+
+# Don't use ruby keywords
+
+age = 21
+puts age
+
+name = "Cody"
+last_name = "Hundi"
+puts name
+puts last_name
+
+puts age + 7 # 21 + 7
+puts name + last_name
+
+age = 22 # after this point age will equal 22
+
