9b1c7c71be90915169b8e3709406edf2f5a99dfd
Cody Wiley <cwiley.cs@gmail.com>
2024-02-05 12:53:07 -0500

    Create Exceptions_nameerror.rb

---
+++ week-4/Exceptions_nameerror.rb
@@ -0,0 +1,8 @@
+puts favorite_food
+
+# outputs error since there's no variable called favorite_food
+
+favorite_food = "baked spaghetti"
+puts favoritefood
+
+# outputs error because of spelling error
