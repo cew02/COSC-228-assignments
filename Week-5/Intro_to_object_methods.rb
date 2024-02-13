00f464b2a930da3da8a62a7298b8ba99033405b8
Cody Wiley <cwiley.cs@gmail.com>
2024-02-13 12:40:55 -0500

    Create Intro_to_object_methods.rb

---
+++ Week-5/Intro_to_object_methods.rb
@@ -0,0 +1,8 @@
+puts "hello world".length # length is a method
+# hello world = 5 + 5 + 1 (the space is one) = 11
+puts "hello world".upcase
+# upcase in all caps
+puts "TOTALLY DIFFERENT".downcase
+
+expression = "TOTALLY DIFFERENT"
+puts expression.downcase
