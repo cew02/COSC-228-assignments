e68b21409910384d50dae00ebd2c25eeb718c11c
Cody Wiley <cwiley.cs@gmail.com>
2024-02-13 12:56:58 -0500

    Update Milkshake.rb

---
+++ Week-5/Milkshake.rb
@@ -0,0 +1,5 @@
+puts "Hello, what is your name?"
+name = gets.chomp
+puts "How many milkshakes would you like, #{name}?"
+milkshake = gets.chomp
+puts "Thank you, #{name}! You have ordered #{milkshake} milkshakes"
