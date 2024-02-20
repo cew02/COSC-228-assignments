038cfa1bedf4dcf55a954e0a45e66c1c71ae6082
Cody Wiley <cwiley.cs@gmail.com>
2024-02-20 15:35:17 -0500

    Create parameters_and_arguments.rb

---
+++ week-6/parameters_and_arguments.rb
@@ -0,0 +1,13 @@
+def praise_person(name)
+    puts "#{name} is amazing"
+    puts "#{name} is charming"
+    puts "#{name} is talented"
+    puts "#{name} is a good boy"
+end
+
+praise_person("Boris")
+praise_person("Bobby")
+praise_person "Debbie"
+
+# praise_person
+praise_person("Debbie", "Ben")
