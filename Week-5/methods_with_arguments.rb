31fb01c4a7726324683a9df1ed9fc3af3b037c55
Cody Wiley <cwiley.cs@gmail.com>
2024-02-20 14:46:43 -0500

    Create methods_with_arguments.rb

---
+++ Week-5/methods_with_arguments.rb
@@ -0,0 +1,9 @@
+puts "Big Mac".include?("B")
+puts "Big Mac".include?("M")
+puts "Big Mac".include?("z")
+puts "Big Mac".include?("b")
+# puts "Big Mac".include?
+# pus "Big Mac".include? "A", "B"
+
+puts "Double Whopper"
+puts("Double Whoper", "Triple Whopper")
