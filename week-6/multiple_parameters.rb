26c705fb5ef56002d2d3058a71da856b0810437a
Cody Wiley <cwiley.cs@gmail.com>
2024-02-27 12:39:18 -0500

    Create multiple_parameters.rb

---
+++ week-6/multiple_parameters.rb
@@ -0,0 +1,10 @@
+def praise_person(name, age)
+    puts "#{name} is amazing"
+    puts "#{name} is charming"
+    puts "#{name} is talented"
+    puts "#{name} is A good boy"
+    puts "They are #{age} years old"
+end
+
+praise_person("Camron", 21)
+praise_person("Cody", 22)
