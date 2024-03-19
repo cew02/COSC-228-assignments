14a7c196177d399f03416e7a55eec851d30686fc
Cody Wiley <cwiley.cs@gmail.com>
2024-03-19 12:08:03 -0400

    Create coding_challenge.rb

---
+++ week-10/coding_challenge.rb
@@ -0,0 +1,14 @@
+# Define a print_five_times method that accepts no arguments.
+# It should output the text "Hello" 5 times using the time method.
+# Use the print method in your solution
+#
+# Examples
+#
+# print_five_times() should output "HelloHelloHelloHelloHello"
+
+def print_five_times
+    5.times { print "Hello" }
+end
+
+print_five_times
+puts
