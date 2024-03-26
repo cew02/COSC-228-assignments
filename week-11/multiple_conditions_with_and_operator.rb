bfa7de689ff8d04d8d91e336112119cf5772aa0d
Cody Wiley <cwiley.cs@gmail.com>
2024-03-26 15:05:31 -0400

    Create multiple_conditions_with_and_operator.rb

---
+++ week-11/multiple_conditions_with_and_operator.rb
@@ -0,0 +1,12 @@
+# &&
+# AND
+
+puts "Please enter username"
+username = gets.chomp
+puts "Please enter password"
+password = gets.chomp
+if username == "rubydev1" && password == "topsecret" && condition3
+    puts "congrats, you've logged in!"
+else
+    puts "Indorrect, no access for you!"
+end
